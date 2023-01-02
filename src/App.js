8c409123c701267dd21e54b1c1926052bf255509
Tran Ong <baotranongtran@gmail.com>
2023-01-01 20:24:25 -0500

    new updates-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Products from './pages/products/products';
-import {BrowserRouter, Switch, Link, Route, Router} from 'react-router-dom'
-import {items} from '../src/data/list'
+import {Switch, Route} from 'react-router-dom'
 import Cart from './pages/cart/cart';
 import Product from './pages/product/product';
 import LeftComponents from './components/navigation/leftcomponents';
-import { StoreProvider, StoreContext } from '../src/context/context'
 import Home from './pages/homepage/home/home';
 import About from './pages/homepage/about/about';
 import Contact from './pages/homepage/contact/contact';
@@ -14,8 +11,7 @@ import Contact from './pages/homepage/contact/contact';
 function App() {
   /*const [cart, setCart] = useState([])
   console.log(cart)
-  */
- const children = {Cart, Product}
+  
   /*return (
     <div>
       <LeftComponents />
